3e353ca2a8e6c9f8f1a0211bd481c97aca13651c
Cauã Henrique <92392798+caffe0@users.noreply.github.com>
2021-11-01 17:07:56 -0300

    Update movimentacao.cs

--- movimentacao.cs
+++ movimentacao.cs
@@ -4,46 +4,36 @@ using UnityEngine;
 
 public class ThirsdPersonMovementScript : MonoBehaviour
 {
-
-    //criacao de variaveis
-
+    Animator anim;
     public CharacterController controller;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     public Transform cam;
 
-
-    //animação
-
-    public Animator animator;
-    int moveXAnimatorParameterId;
-    int moveZAnimatorParameterId;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
-        animator = GetComponent<Animator>();
-        moveXAnimatorParameterId = Animator.StringToHash("MoveX");
-        moveZAnimatorParameterId = Animator.StringToHash("MoveZ");
-
-        float horizontal = Input.GetAxisRaw("Horizontal"); //input da tecla horizontal
-        if(horizontal != 0f);
-        {
-            animator.SetFloat(moveXAnimatorParameterId, horizontal);
-        }
-        float vertical = Input.GetAxisRaw("Vertical"); //input da tecla vertical
-        if(vertical != 0f);
-        {
-            animator.SetFloat(moveZAnimatorParameterId, vertical);
-        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        //mivimentacao
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        anim.SetBool("isMoving", isWalking);
+
+
 
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+            //movimentar junto da camera
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
